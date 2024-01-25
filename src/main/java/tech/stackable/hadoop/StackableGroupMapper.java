8f501179aa7f94bcc5f725df7d86adde11412ef2
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2024-01-25 15:57:29 +0100

    replace fabric calls with http

--- src/main/java/tech/stackable/hadoop/StackableGroupMapper.java
+++ src/main/java/tech/stackable/hadoop/StackableGroupMapper.java
@@ -1,27 +1,30 @@
 package tech.stackable.hadoop;
 
-import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.dsl.MixedOperation;
-import io.fabric8.kubernetes.client.dsl.Resource;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.security.GroupMappingServiceProvider;
 import org.apache.hadoop.util.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StackableGroupMapper implements GroupMappingServiceProvider {
-    private static final String USERS_MAPPING_SUFFIX = "USERS_MAPPING_SUFFIX";
-    private static final String USERS_MAPPING_TARGET = "USERS_MAPPING_TARGET";
+    private static final String OPA_MAPPING_URL_PROP = "hadoop.security.group.mapping.opa.url";
     private final Logger LOG = LoggerFactory.getLogger(StackableGroupMapper.class);
     private final KubernetesClient client;
+    private final Configuration configuration;
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
     public StackableGroupMapper() {
         this.client = new DefaultKubernetesClient();
+        this.configuration = new Configuration();
     }
 
     /**
@@ -34,51 +37,33 @@ public class StackableGroupMapper implements GroupMappingServiceProvider {
     public List<String> getGroups(String user) throws IOException {
         LOG.info("Calling StackableGroupMapper.getGroups...");
 
-        String mappingSuffix = System.getenv(USERS_MAPPING_SUFFIX); // e.g. "v1/data/hdfs"
-        String mappingTarget = System.getenv(USERS_MAPPING_TARGET); // e.g. "users" in {"result":{"users":"me,myself,I"}}
+        String opaMappingUrl = configuration.get(OPA_MAPPING_URL_PROP);
 
-        LOG.info("Detected USERS_MAPPING_SUFFIX: [{}] and USERS_MAPPING_TARGET: [{}]", mappingSuffix, mappingTarget);
+        if (opaMappingUrl == null) {
+            throw new RuntimeException("Config \"" + OPA_MAPPING_URL_PROP + "\" missing");
+        }
 
-        List<ConfigMap> discovery = client
-                .configMaps()
-                .withLabel("app.kubernetes.io/component", "server")
-                .withLabel("app.kubernetes.io/name", "opa")
-                .withLabel("app.kubernetes.io/role-group", "discovery")
-                .list()
-                .getItems();
-        LOG.info("Retrieved discovery config-map [{}]",
-                discovery.stream().map(cm -> cm.getMetadata().getName()).collect(Collectors.toList()));
+        URI opaUri = URI.create(opaMappingUrl);
+        HttpResponse<String> response = null;
+        try {
+            response = httpClient.send(
+                    HttpRequest.newBuilder(opaUri).header("Content-Type", "application/json").GET().build(),
+                    //.POST(HttpRequest.BodyPublishers.ofByteArray(user.getBytes())).build(),
+                    HttpResponse.BodyHandlers.ofString());
+            LOG.info("Opa response [{}]", response);
+        } catch (InterruptedException e) {
+            LOG.error(e.getMessage());
+        }
 
-        List<Node> nodes = client.nodes().list().getItems();
-        LOG.info("Retrieved nodes: " + nodes.stream().map(node -> node.getMetadata().getName()).collect(Collectors.toList()));
+        switch (response.statusCode()) {
+            case 200:
+                break;
+            default:
+                throw new IOException(opaUri.toString());
+        }
+        String responseBody = response.body();
+        LOG.info("Response body [{}]", responseBody);
 
-        String url = discovery.get(0).getData().get("OPA");
-        LOG.info("Service: [{}]", url);
-
-
-        List<ConfigMap> regoBundle = client
-                .configMaps()
-                .withLabel("opa.stackable.tech/bundle", "hdfs-group-mapping")
-                .list()
-                .getItems();
-        LOG.info("Retrieved rego config-map [{}]",
-                regoBundle.stream().map(cm -> cm.getMetadata().getName()).collect(Collectors.toList()));
-        String apiVersion = regoBundle.get(0).getApiVersion();
-
-        Service opa = client
-                .services()
-                .withLabel("app.kubernetes.io/name", "opa")
-                .withLabel("app.kubernetes.io/component", "server")
-                .withLabel("app.kubernetes.io/role-group", "default")
-                .list()
-                .getItems().get(0);
-
-        //String serviceURL = client.services().inNamespace("default").withName(opa.getMetadata().getName())
-        //        .getURL(apiVersion + "/data/hdfs");
-
-        MixedOperation<Endpoints, EndpointsList, DoneableEndpoints, Resource<Endpoints, DoneableEndpoints>> endpoints =
-                client.endpoints();
-        LOG.info("EPs [{}]", endpoints);
         return Lists.newArrayList("me", "myself", "I");
     }
 
@@ -88,6 +73,7 @@ public class StackableGroupMapper implements GroupMappingServiceProvider {
     @Override
     public void cacheGroupsRefresh() {
         // does nothing in this provider of user to groups mapping
+        LOG.info("cacheGroupsRefresh: caching should be provided by the policy provider");
     }
 
     /**
@@ -98,5 +84,6 @@ public class StackableGroupMapper implements GroupMappingServiceProvider {
     @Override
     public void cacheGroupsAdd(List<String> groups) {
         // does nothing in this provider of user to groups mapping
+        LOG.info("cacheGroupsAdd: caching should be provided by the policy provider");
     }
 }
