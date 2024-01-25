2f5a2b1f681827b6a15aa18f686d29530f45d22c
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2024-01-25 14:19:07 +0100

    wip: retrieve rego rule

--- src/main/java/tech/stackable/hadoop/StackableGroupMapper.java
+++ src/main/java/tech/stackable/hadoop/StackableGroupMapper.java
@@ -1,21 +1,29 @@
 package tech.stackable.hadoop;
 
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
 import org.apache.hadoop.security.GroupMappingServiceProvider;
+import org.apache.hadoop.util.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StackableGroupMapper implements GroupMappingServiceProvider {
-    private KubernetesClient client;
+    private static final String USERS_MAPPING_SUFFIX = "USERS_MAPPING_SUFFIX";
+    private static final String USERS_MAPPING_TARGET = "USERS_MAPPING_TARGET";
     private final Logger LOG = LoggerFactory.getLogger(StackableGroupMapper.class);
+    private final KubernetesClient client;
 
     public StackableGroupMapper() {
         this.client = new DefaultKubernetesClient();
     }
+
     /**
      * Returns list of groups for a user.
      *
@@ -24,7 +32,54 @@ public class StackableGroupMapper implements GroupMappingServiceProvider {
      */
     @Override
     public List<String> getGroups(String user) throws IOException {
-        return null;
+        LOG.info("Calling StackableGroupMapper.getGroups...");
+
+        String mappingSuffix = System.getenv(USERS_MAPPING_SUFFIX); // e.g. "v1/data/hdfs"
+        String mappingTarget = System.getenv(USERS_MAPPING_TARGET); // e.g. "users" in {"result":{"users":"me,myself,I"}}
+
+        LOG.info("Detected USERS_MAPPING_SUFFIX: [{}] and USERS_MAPPING_TARGET: [{}]", mappingSuffix, mappingTarget);
+
+        List<ConfigMap> discovery = client
+                .configMaps()
+                .withLabel("app.kubernetes.io/component", "server")
+                .withLabel("app.kubernetes.io/name", "opa")
+                .withLabel("app.kubernetes.io/role-group", "discovery")
+                .list()
+                .getItems();
+        LOG.info("Retrieved discovery config-map [{}]",
+                discovery.stream().map(cm -> cm.getMetadata().getName()).collect(Collectors.toList()));
+
+        List<Node> nodes = client.nodes().list().getItems();
+        LOG.info("Retrieved nodes: " + nodes.stream().map(node -> node.getMetadata().getName()).collect(Collectors.toList()));
+
+        String url = discovery.get(0).getData().get("OPA");
+        LOG.info("Service: [{}]", url);
+
+
+        List<ConfigMap> regoBundle = client
+                .configMaps()
+                .withLabel("opa.stackable.tech/bundle", "hdfs-group-mapping")
+                .list()
+                .getItems();
+        LOG.info("Retrieved rego config-map [{}]",
+                regoBundle.stream().map(cm -> cm.getMetadata().getName()).collect(Collectors.toList()));
+        String apiVersion = regoBundle.get(0).getApiVersion();
+
+        Service opa = client
+                .services()
+                .withLabel("app.kubernetes.io/name", "opa")
+                .withLabel("app.kubernetes.io/component", "server")
+                .withLabel("app.kubernetes.io/role-group", "default")
+                .list()
+                .getItems().get(0);
+
+        //String serviceURL = client.services().inNamespace("default").withName(opa.getMetadata().getName())
+        //        .getURL(apiVersion + "/data/hdfs");
+
+        MixedOperation<Endpoints, EndpointsList, DoneableEndpoints, Resource<Endpoints, DoneableEndpoints>> endpoints =
+                client.endpoints();
+        LOG.info("EPs [{}]", endpoints);
+        return Lists.newArrayList("me", "myself", "I");
     }
 
     /**
