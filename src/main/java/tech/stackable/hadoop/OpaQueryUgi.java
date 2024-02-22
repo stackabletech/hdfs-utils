0d7f41b8d1d26b032dcf2ae03a8c81795f85489c
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2024-02-22 11:43:37 +0100

    fix: Fix serialization issues during impersonation

--- src/main/java/tech/stackable/hadoop/OpaQueryUgi.java
+++ src/main/java/tech/stackable/hadoop/OpaQueryUgi.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 import java.util.List;
 
 public class OpaQueryUgi {
-    public UserGroupInformation realUser;
+    // Wrapping this
+    public OpaQueryUgi realUser;
     public String userName;
     public String shortUserName;
 
@@ -16,8 +17,18 @@ public class OpaQueryUgi {
     public UserGroupInformation.AuthenticationMethod authenticationMethod;
     public UserGroupInformation.AuthenticationMethod realAuthenticationMethod;
 
+    /**
+     * Wrapper around {@link UserGroupInformation}, which does not throw random errors during serialization when no primary
+     * group is known for the user.
+     * "Caused by: com.fasterxml.jackson.databind.JsonMappingException: Unexpected IOException (of type java.io.IOException): There is no primary group for UGI hive/hive-iceberg.default.svc.cluster.local@KNAB.COM (auth:KERBEROS)"
+     */
     public OpaQueryUgi(UserGroupInformation ugi) {
-        this.realUser = ugi.getRealUser();
+        UserGroupInformation realUser = ugi.getRealUser();
+        if (realUser != null) {
+            this.realUser = new OpaQueryUgi(ugi.getRealUser());
+        } else {
+            this.realUser = null;
+        }
         this.userName = ugi.getUserName();
         this.shortUserName = ugi.getShortUserName();
         try {
